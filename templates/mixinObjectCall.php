8611005be3df537d36afe5bf21f4ebac8e7aece2
Toha <tohenk@yahoo.com>
2025-05-10 16:28:55 +0700

    Move templates outside source folder.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- templates/mixinObjectCall.php
+++ templates/mixinObjectCall.php
@@ -1,4 +1,5 @@
 if ($callable = <?= $method ?>('<?= $class ?>:'.$name)) {
     array_unshift($params, $this);
+
     return call_user_func_array($callable, $params);
 }
--- templates/mixinPreDelete.php
+++ templates/mixinPreDelete.php
@@ -1,6 +1,7 @@
 foreach (<?= $method ?>('<?= $class ?>:delete:pre') as $callable) {
     if (call_user_func($callable, $this, $con)) {
         $con->commit();
+
         return;
     }
 }
